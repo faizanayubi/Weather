7f70cc9dc5b9a24f93ad8d0a87d5992240c05366
faizanayubi <indianayubi@gmail.com>
2015-07-27 15:51:58 +0530

    Added DB Create Test

---
+++ app/src/androidTest/java/org/swiftdeal/weather/app/test/fullTestSuite.java
@@ -0,0 +1,18 @@
+package org.swiftdeal.weather.app.test;
+
+import android.test.suitebuilder.TestSuiteBuilder;
+
+import junit.framework.Test;
+
+/**
+ * Created by Faizan Ayubi on 27-07-2015.
+ */
+public class fullTestSuite {
+    public static Test suite() {
+        return new TestSuiteBuilder(fullTestSuite.class).includeAllPackagesUnderHere().build();
+    }
+
+    public fullTestSuite() {
+        super();
+    }
+}
