9081656e91b0e35575f21fc80f5d3fdc6ad15e54
faizanayubi <indianayubi@gmail.com>
2015-07-28 22:34:34 +0530

    Content Provider added

--- app/src/main/java/org/swiftdeal/weather/data/WeatherContract.java
+++ app/src/main/java/org/swiftdeal/weather/data/WeatherContract.java
@@ -1,12 +1,27 @@
 package org.swiftdeal.weather.data;
 
+import android.content.ContentUris;
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 /**
  * Created by Faizan Ayubi on 27-07-2015.
  */
 public class WeatherContract {
+
+    public static final String CONTENT_AUTHORITY = "org.swiftdeal.weather";
+
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    public static final String PATH_WEATHER = "weather";
+    public static final String PATH_LOCATION = "location";
+
     public static final class WeatherEntry implements BaseColumns {
+
+        public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon().appendPath(PATH_WEATHER).build();
+
+        public static final String CONTENT_TYPE = "vnd.android.cursor.dir/" + CONTENT_AUTHORITY + "/" + PATH_WEATHER;
+        public static final String CONTENT_ITEM_TYPE = "vnd.android.cursor.item/" + CONTENT_AUTHORITY + "/" + PATH_WEATHER;
 
         public static final String TABLE_NAME = "weather";
 
@@ -27,9 +42,43 @@ public class WeatherContract {
         public static final String COLUMN_WIND_SPEED = "wind";
 
         public static final String COLUMN_DEGREES = "degrees";
+
+        public static Uri buildWeatherUri(long id) {
+            return ContentUris.withAppendedId(CONTENT_URI, id);
+        }
+
+        public static Uri buildWeatherLocation(String locationSettings) {
+            return CONTENT_URI.buildUpon().appendPath(locationSettings).build();
+        }
+
+        public static Uri buildWeatherLocationWithStartDate(String locationSettings, String startDate) {
+            return CONTENT_URI.buildUpon().appendPath(locationSettings).appendQueryParameter(COLUMN_DATETEXT, startDate).build();
+        }
+
+        public static Uri buildWeatherLocationWithDate(String locationSettings, String date) {
+            return CONTENT_URI.buildUpon().appendPath(locationSettings).appendPath(date).build();
+        }
+
+        public static String getLocationSettingfromUri(Uri uri) {
+            return uri.getPathSegments().get(1);
+        }
+
+        public static String getDateFromUri(Uri uri) {
+            return uri.getPathSegments().get(2);
+        }
+
+        public static String getStartDateFromUri(Uri uri) {
+            return uri.getQueryParameter(COLUMN_DATETEXT);
+        }
+
     }
 
     public static final class LocationEntry implements BaseColumns {
+
+        public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon().appendPath(PATH_LOCATION).build();
+
+        public static final String CONTENT_TYPE = "vnd.android.cursor.dir/" + CONTENT_AUTHORITY + "/" + PATH_LOCATION;
+        public static final String CONTENT_ITEM_TYPE = "vnd.android.cursor.item/" + CONTENT_AUTHORITY + "/" + PATH_LOCATION;
 
         public static final String TABLE_NAME = "location";
 
@@ -40,6 +89,8 @@ public class WeatherContract {
         public static final String COLUMN_COORD_LAT = "coord_lat";
         public static final String COLUMN_COORD_LONG = "coord_long";
 
-
+        public static Uri buildLocationUri(long id) {
+            return ContentUris.withAppendedId(CONTENT_URI, id);
+        }
     }
 }
