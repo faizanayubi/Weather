4dcd446e756ec37ab64f10ca3260148da9df68e6
faizanayubi <indianayubi@gmail.com>
2015-07-28 14:39:12 +0530

    Tests Added

--- app/src/androidTest/java/org/swiftdeal/weather/app/test/TestDb.java
+++ app/src/androidTest/java/org/swiftdeal/weather/app/test/TestDb.java
@@ -1,18 +1,102 @@
 package org.swiftdeal.weather.app.test;
 
+import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.test.AndroidTestCase;
+import android.util.Log;
 
+import org.swiftdeal.weather.data.WeatherContract;
 import org.swiftdeal.weather.data.WeatherDbHelper;
+
+import java.sql.SQLData;
 
 /**
  * Created by Faizan Ayubi on 27-07-2015.
  */
 public class TestDb extends AndroidTestCase {
+
+    private final String LOG_TAG = TestDb.class.getSimpleName();
+
     public void testCreateDb() throws Throwable {
         mContext.deleteDatabase(WeatherDbHelper.DATABASE_NAME);
         SQLiteDatabase db = new WeatherDbHelper(this.mContext).getWritableDatabase();
         assertEquals(true, db.isOpen());
         db.close();
+    }
+
+    public void testInsertReadDb() {
+        String testName = "North Pole";
+        String testLocationSetting = "99705";
+        double testLatitude = 64.772;
+        double testLongitude = -147.355;
+
+        WeatherDbHelper dbHelper = new WeatherDbHelper(mContext);
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(WeatherContract.LocationEntry.COLUMN_CITY_NAME, testName);
+        values.put(WeatherContract.LocationEntry.COLUMN_LOCATION_SETTINGS, testLocationSetting);
+        values.put(WeatherContract.LocationEntry.COLUMN_COORD_LAT, testLatitude);
+        values.put(WeatherContract.LocationEntry.COLUMN_COORD_LONG, testLongitude);
+
+        long locationRowId;
+        locationRowId = db.insert(WeatherContract.LocationEntry.TABLE_NAME, null, values);
+
+        assertTrue(locationRowId != -1);
+        Log.d(LOG_TAG, "New row id: " + locationRowId);
+
+        String[] columns = {
+                WeatherContract.LocationEntry._ID,
+                WeatherContract.LocationEntry.COLUMN_LOCATION_SETTINGS,
+                WeatherContract.LocationEntry.COLUMN_CITY_NAME,
+                WeatherContract.LocationEntry.COLUMN_COORD_LAT,
+                WeatherContract.LocationEntry.COLUMN_COORD_LONG
+        };
+
+        Cursor cursor = db.query(
+                WeatherContract.LocationEntry.TABLE_NAME,
+                columns,
+                null,
+                null,
+                null,
+                null,
+                null
+        );
+
+        if (cursor.moveToFirst()) {
+            int locationIndex = cursor.getColumnIndex(WeatherContract.LocationEntry.COLUMN_LOCATION_SETTINGS);
+            String location = cursor.getString(locationIndex);
+
+            int nameIndex = cursor.getColumnIndex(WeatherContract.LocationEntry.COLUMN_CITY_NAME);
+            String name = cursor.getString(nameIndex);
+
+            int latIndex = cursor.getColumnIndex(WeatherContract.LocationEntry.COLUMN_COORD_LAT);
+            String latitude = cursor.getString(latIndex);
+
+            int longIndex = cursor.getColumnIndex(WeatherContract.LocationEntry.COLUMN_COORD_LONG);
+            String longitude = cursor.getString(longIndex);
+
+            assertEquals(testName, name);
+            assertEquals(testLocationSetting, location);
+            assertEquals(testLatitude, latitude);
+            assertEquals(testLongitude, longitude);
+
+            ContentValues weatherValues = new ContentValues();
+            weatherValues.put(WeatherContract.WeatherEntry.COLUMN_LOC_KEY, locationRowId);
+            weatherValues.put(WeatherContract.WeatherEntry.COLUMN_DATETEXT, "20141205");
+            weatherValues.put(WeatherContract.WeatherEntry.COLUMN_DEGREES, 1.1);
+            weatherValues.put(WeatherContract.WeatherEntry.COLUMN_HUMIDITY, 1.2);
+            weatherValues.put(WeatherContract.WeatherEntry.COLUMN_PRESSURE, 1.3);
+            weatherValues.put(WeatherContract.WeatherEntry.COLUMN_MAX_TEMP, 75);
+            weatherValues.put(WeatherContract.WeatherEntry.COLUMN_MIN_TEMP, 65);
+            weatherValues.put(WeatherContract.WeatherEntry.COLUMN_SHORT_DESC, "Asteroids");
+            weatherValues.put(WeatherContract.WeatherEntry.COLUMN_WIND_SPEED, 5.5);
+            weatherValues.put(WeatherContract.WeatherEntry.COLUMN_WEATHER_ID, 321);
+
+        } else {
+            fail("No values returned : (");
+        }
+
     }
 }
